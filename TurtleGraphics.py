79ad4dfd2db8d6df1ea7c9bfed334c746999afee
fatima-davila <fdavila@unomaha.edu>
2025-02-16 20:47:40 -0600

    finished

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -29,6 +29,23 @@ def fillCorner(felipe, corner):
         felipe.begin_fill()
         drawSquare(felipe, 50)
         felipe.end_fill()
+    elif corner == 3:
+        felipe.forward(100)
+        felipe.right(90)
+        felipe.forward(50)
+        felipe.beginfill()
+        drawSquare(felipe,50)
+        felipe.end_fill()
+    elif corner == 4:
+        felipe.forward(100)
+        felipe.right(90)
+        felipe.forward(100)
+        felipe.right(90)
+        felipe.forward(50)
+        felipe.beginfill()
+        drawSquare(felipe,50)
+        felipe.end_fill()
+        
     
 def main():
     myTurtle = turtle.Turtle()
@@ -42,6 +59,15 @@ def main():
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+def squaresinSquares(doc, squares) :
+    cool = -199
+    dude = 199
+    for o in range(squares):
+        doc.penup()
+        doc.goto( cool, dude)
+        drawSquare(doc,dude *2)
+        cool = cool + 10
+        dude = dude - 10
     # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
 
 
